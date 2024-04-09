03509adc3b03b6d4fe90b726ac187fbb219bb8b2
Gregoire GUEMAS <gregoire.guemas@navispeed.eu>
2024-04-09 17:04:26 +0200

    Add countdown limit

--- api/src/api.py
+++ api/src/api.py
@@ -1,9 +1,13 @@
 import os
 
+
 from fastapi import FastAPI
+from starlette.responses import Response
 
 from calculator.models.empire import Empire
 from service import Service
+
+MAX_DURATION = int(os.environ["MAX_DURATION"])
 
 app = FastAPI()
 
@@ -16,5 +20,8 @@ def root():
 
 
 @app.get("/route")
-def route(empire: Empire):
+def route(empire: Empire, response: Response):
+    if empire["countdown"] > MAX_DURATION:
+        response.status_code = 400
+        return {"message": f"Countdown must be less than {MAX_DURATION}"}
     return service.navigate(empire)
